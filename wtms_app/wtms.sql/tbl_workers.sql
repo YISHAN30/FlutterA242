3532b8d831e0085f3b3dc30025802f511ddc36a7
YISHAN30 <leeyishan30@gmail.com>
2025-05-11 11:53:54 +0800

    initial commit

--- wtms_app/wtms.sql/tbl_workers.sql
+++ wtms_app/wtms.sql/tbl_workers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 11, 2025 at 05:48 AM
+-- Generation Time: May 11, 2025 at 05:51 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -35,13 +35,6 @@ CREATE TABLE `tbl_workers` (
   `phone` varchar(20) NOT NULL,
   `address` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `tbl_workers`
---
-
-INSERT INTO `tbl_workers` (`id`, `full_name`, `email`, `password`, `phone`, `address`) VALUES
-(1, 'Angel\r\n', 'angel123@gmail.com', 'edf360b3f9f25e1b43f3777db55c002035dcfe5c', '011-23456789', '123, Megan Road, Hana Park 12345 Klang, Selangor');
 
 --
 -- Indexes for dumped tables
